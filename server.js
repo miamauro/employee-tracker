13bd19b33b116ee0a16ec7dad4998f3cd3095276
miamauro <miiamauro@outlook.com>
2023-03-15 01:10:00 -0500

    created connection to database

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "employees_db",
+});
