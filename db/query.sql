cba6d5e44a4259e4e9ca7f10f2798407264beaef
miamauro <miiamauro@outlook.com>
2023-03-16 20:29:57 -0500

    added manager id to view all employees table

--- db/query.sql
+++ db/query.sql
@@ -7,7 +7,7 @@ LEFT JOIN department
 ON role.department_id = department.id;
 
 --View all employees--
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee 
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee 
 LEFT JOIN role 
 ON employee.role_id = role.id 
 LEFT JOIN department 
--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function viewAllRoles() {
 
 function viewAllEmployees() {
   db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
     (err, results) => {
       console.table(results);
       init();
